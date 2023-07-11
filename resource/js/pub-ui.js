5beaf250bf40d2e056a11817333931e364f23175
miok <aluvy_@naver.com>
2023-07-11 16:09:31 +0900

    fullH 수정

--- resource/js/pub-ui.js
+++ resource/js/pub-ui.js
@@ -65,10 +65,6 @@ var pubUi = {
             }
         });
 
-        
-        pubUi.getFullH();
-        $(window).on("resize", pubUi.getFullH);
-
         pubUi.select();
         pubUi.sortLayer();
         pubUi.tooltip();
@@ -82,13 +78,6 @@ var pubUi = {
         acdItem.init();
         scrollFixed.init();
         
-    },
-    getFullH : function(){
-        const fullH = $(document).find(".fullH");
-        if( fullH.length < 1 ) return;
-
-        const docH = $(window).height();
-        $(document).find(".fullH").css({"min-height": `${docH}px`});
     },
     select : function(){
         $(document).on('click', '.select_list button, .select_list02 button', function(){
